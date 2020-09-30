0be1340bf35d2a469dc7e7729acee211f4dc9746
CaseyMartin23 <cm232311@gmail.com>
2020-09-30 13:46:03 +0200

    Hopefully get shit done today

--- back-end/server/server.ts
+++ back-end/server/server.ts
@@ -16,6 +16,7 @@ const port = process.env.PORT || 5000;
 
 const { queries } = require("../db/queries");
 
+// Initialize Passport.js
 const { initializePassport } = require("./passport-config");
 initializePassport(
   passport,
@@ -23,9 +24,8 @@ initializePassport(
   (id: number) => queries.users.getUserById(id)
 );
 
-// app.use(express.static(path.join(__dirname, "/../../front-end/build")));
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(
   session({
@@ -37,9 +37,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.get("*", (req: any, res: any) =>
-//   res.sendFile(path.resolve(__dirname + "/../../front-end/build/index.html"))
-// );
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resovle(__dirname, "/../../front-end/build")));
+}
 
 app.post(
   "/signin",
@@ -65,20 +65,6 @@ app.post("/signup", async (req: Request, res: Response) => {
     console.log("\n***Error***:\n\t", err.message);
     res.status(409).send(JSON.stringify(err.message));
   }
-});
-
-// app.get("/test", (req: any, res: any) => {
-//   console.log("TeSTinG....\nIt works!!!");
-//   res.send("");
-// });
-
-app.get("/isUserAuthed", async (req: Request, res: Response) => {
-  const isAuthed = (await req.isAuthenticated())
-    ? await req.isAuthenticated()
-    : false;
-
-  res.send(JSON.stringify(isAuthed));
-  res.end();
 });
 
 app.listen(port, () => console.log(`Server is Listening on port: ${port}...`));
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
+
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
 } from "react-router-dom";
-import userAuthenticationContext, {
-  isUserAuthed,
-} from "./comps/userAuthenticationContext";
+
 import HomePage from "./pages/homePage";
 import SignInPage from "./pages/signInPage";
 import SignUpPage from "./pages/signUpPage";
 import styled from "styled-components";
-
-type CheckUserAuthProps = {
-  children: React.ReactChild | React.ReactChildren;
-};
 
 const AppBackground = styled.div`
   background-color: #282c34;
@@ -26,46 +21,22 @@ const AppBackground = styled.div`
   padding: 0;
 `;
 
-const CheckUserAuth = (props: CheckUserAuthProps) => {
-  const [isAuthed, setIsAuthed] = useState();
-
-  const getUserAuthStatus = (status: any) => {
-    console.log("getUserAuthStatus-status->>", status);
-    setIsAuthed(status);
-  };
-
-  isUserAuthed(getUserAuthStatus);
-
-  console.log("isAuthed->>", isAuthed);
-
-  if (isAuthed !== undefined && typeof isAuthed === "boolean") {
-    if (!isAuthed) return <Redirect to="/signin" />;
-  }
-  return <React.Fragment>{props.children}</React.Fragment>;
-};
-
 const App = () => {
-  // const [userAuthStat, setUserAuthStat] = useState<any>();
-
   return (
     <Router>
-      <userAuthenticationContext.Provider value={false}>
-        <CheckUserAuth>
-          <AppBackground id="App">
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-              <Route path="/signIn">
-                <SignInPage />
-              </Route>
-              <Route path="/signUp">
-                <SignUpPage />
-              </Route>
-            </Switch>
-          </AppBackground>
-        </CheckUserAuth>
-      </userAuthenticationContext.Provider>
+      <AppBackground id="App">
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/signIn">
+            <SignInPage />
+          </Route>
+          <Route path="/signUp">
+            <SignUpPage />
+          </Route>
+        </Switch>
+      </AppBackground>
     </Router>
   );
 };
