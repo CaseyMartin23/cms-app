4007be43f17269c0dd5086a82f7f19e1337168bb
CaseyMartin23 <cm232311@gmail.com>
2020-09-30 15:42:38 +0200

    Fresh Start

--- back-end/db/knex.js
+++
@@ -1,7 +0,0 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("./knexfile");
-const configedEnvironment = config[environment];
-const knex = require("knex");
-const connection = knex(configedEnvironment);
-
-module.exports = connection;
