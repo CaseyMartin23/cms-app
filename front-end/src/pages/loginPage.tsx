e15f5eb492239d31152bde83db2b356343b054e8
CaseyMartin23 <cm232311@gmail.com>
2020-10-02 12:57:21 +0200

    Created signin signup forms

--- front-end/src/pages/loginPage.tsx
+++ front-end/src/pages/loginPage.tsx
@@ -1,35 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Form, Field } from "react-final-form";
-
-import styled from "styled-components";
-
-const StyledForm = styled.form`
-  background-color: grey;
-`;
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const LoginPage = () => {
+  const [loginFormData, setLoginFormData] = useState({});
+  const [rememberMe, setRememberMe] = useState(true);
+
+  useEffect(() => {
+    console.log("LoginFormData->", loginFormData);
+  }, [loginFormData]);
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    const { name, value } = target;
+
+    if (name === "remember") {
+      setRememberMe(!rememberMe);
+      setLoginFormData({ ...loginFormData, [name]: rememberMe });
+    }
+
+    setLoginFormData({ ...loginFormData, [name]: value });
+  };
+
   return (
-    <div>
-      <Form
-        onSubmit={() => {}}
-        render={({ handleSubmit, form, submitting, pristine, values }) => (
-          <StyledForm onSubmit={handleSubmit}>
-            <Field name="email">
-              {({ input, meta }) => (
-                <div>
-                  <input
-                    {...input}
-                    type="text"
-                    placeholder="Please enter email address"
-                  />
-                </div>
-              )}
-            </Field>
-          </StyledForm>
-        )}
-      />
-    </div>
+    <Container component="main" maxWidth="xs">
+      <div>
+        <Avatar>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form noValidate>
+          <TextField
+            onChange={onChangeHandler}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            onChange={onChangeHandler}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={onChangeHandler}
+                value={rememberMe}
+                name="remember"
+                color="primary"
+              />
+            }
+            label="Remember me"
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="/recovery" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                Don't have an account? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 };
 
--- front-end/src/pages/registerPage.tsx
+++ front-end/src/pages/registerPage.tsx
@@ -1,7 +1,123 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const RegisterPage = () => {
-  return <div>Register Page</div>;
+  const [registerFormData, setRegisterFormData] = useState({});
+
+  useEffect(() => {
+    console.log("FormData->", registerFormData);
+  }, [registerFormData]);
+
+  const onChangeHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    const { name, value } = target;
+
+    setRegisterFormData({ ...registerFormData, [name]: value });
+  };
+
+  const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const resp = await fetch("/login", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(registerFormData),
+      });
+      const result = await resp.json();
+      console.log("result->", result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div>
+        <Avatar>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form onSubmit={onSubmitHandler} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                onChange={onChangeHandle}
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                onChange={onChangeHandle}
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={onChangeHandle}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={onChangeHandle}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}></Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default RegisterPage;
